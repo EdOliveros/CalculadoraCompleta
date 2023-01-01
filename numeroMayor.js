bbc0950a9dcbcd19427805c9cb5ea1a69a47b80b
Ed Oliveros <edgarolimoy98@gmail.com>
2023-01-01 15:42:07 -0500

    he creado la funcion de buscar el numero mayor

--- numeroMayor.js
+++ numeroMayor.js
@@ -1,4 +1,22 @@
 // encontrar el numero mas grande en un array
 
+var numeros = [
+    1, 2, 3, 4, 5, 9
+]
+
+let numeroMasGrande = 0
+
+function numeroMayor(numeros, numeroMasGrande) {
+    
+    
+
+    for (let i = 0; i < numeros.length; i++) {
+        if(numeroMasGrande < numeros[i]){
+            numeroMasGrande = numeros[i]
+        }
+    } console.log(numeroMasGrande)
+
+}
 
 
+numeroMayor(numeros, numeroMasGrande)
